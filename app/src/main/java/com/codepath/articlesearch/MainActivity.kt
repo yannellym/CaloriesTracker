3d052a9882f1f3a3eb6ab5bdba01ff230dd76ff3
yannelly25 <yannellym@gmail.com>
2023-03-26 20:50:02 -0400

    added inputs

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -59,24 +59,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-/*
-When we make API calls, instead of showing that data, we load that data into the database.
-The database becomes our single source of truth.
-* 2 - We need to update any articleDao interaction to run on another thread.
-We can use Kotlin coroutines for this. They can be a lightweight and simple way to
-"launch" some code to run asynchronously on a different thread. In this case,
-we want to attach this coroutine to the lifecycle of our View. Any coroutine
-launched in this scope is cancelled when the Lifecycle is destroyed.
-To specify where the coroutines should run, Kotlin provides three dispatchers that you can use:
-
-Dispatchers.Main - Use this dispatcher to run a coroutine on the main Android thread. This should
-be used only for interacting with the UI and performing quick work. Examples include calling suspend
-functions, running Android UI framework operations, and updating LiveData objects. The default if no
-dispatcher is specified. Dispatchers.IO - This dispatcher is optimized to perform disk or network
-I/O outside of the main thread. Examples include using the Room component, reading from or writing
-to files, and running any network operations.
-Dispatchers.Default - This dispatcher is optimized to perform CPU-intensive work outside of the
-main thread. Example use cases include sorting a list and parsing JSON.
-Since we are using Room and writing to files, we want to use the IO Dispatcher.
-*/
